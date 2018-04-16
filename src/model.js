8cea14623823363f7f5ab73ecffa98829ffdecea
Jakub Kozak <kozak.jakub55@gmail.com>
2018-04-16 22:24:04 +0200

    add and change tracks for individual instruments

--- src/model.js
+++ src/model.js
@@ -7,14 +7,43 @@ import samples from "./samples.json";
 export function initTracks() {
   return {
     "beats": [
-      {id: 1, name: "hihat-reso", vol: .4, muted: false, beats: initBeats(16)},
-      {id: 2, name: "hihat-plain", vol: .4, muted: false, beats: initBeats(16)},
-      {id: 3, name: "snare-vinyl01", vol: .9, muted: false, beats: initBeats(16)},
-      {id: 4, name: "kick-electro01", vol: .8, muted: false, beats: initBeats(16)},
+      {id: 1, name: "beats/hihat-reso", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 2, name: "beats/hihat-plain", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 3, name: "beats/snare-vinyl01", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 4, name: "beats/kick-electro01", vol: 1, muted: false, beats: initBeats(16)}
     ],
-    "guitar": [],
-    "drums": [],
-    "piano": []
+    "accoustic_guitar": [
+      {id: 1, name: "accoustic_guitar/A2A3", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 2, name: "accoustic_guitar/A3", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 3, name: "accoustic_guitar/B2", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 4, name: "accoustic_guitar/B3", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 5, name: "accoustic_guitar/B3B4", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 6, name: "accoustic_guitar/B4", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 7, name: "accoustic_guitar/C#3", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 8, name: "accoustic_guitar/C#3C#4", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 9, name: "accoustic_guitar/C#4", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 10, name: "accoustic_guitar/D#4", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 11, name: "accoustic_guitar/D3", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 12, name: "accoustic_guitar/D5", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 13, name: "accoustic_guitar/E2", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 14, name: "accoustic_guitar/E4", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 15, name: "accoustic_guitar/F2", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 16, name: "accoustic_guitar/F2F3", vol: 1, muted: false, beats: initBeats(16)},
+    ],
+    "piano": [
+      {id: 1, name: "piano/a", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 2, name: "piano/b", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 3, name: "piano/c", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 4, name: "piano/d", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 5, name: "piano/e", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 6, name: "piano/f", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 7, name: "piano/g", vol: 1, muted: false, beats: initBeats(16)}
+    ],
+    "drums": [
+      {id: 1, name: "drums/kick", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 2, name: "drums/snare", vol: 1, muted: false, beats: initBeats(16)},
+      {id: 3, name: "drums/hat", vol: 1, muted: false, beats: initBeats(16)}
+    ],
   }
 }
 
@@ -23,14 +52,10 @@ export function initBeats(n: number): boolean[] {
 }
 
 export function addTrack(tracks, category) {
+
   const id = Math.max.apply(null, tracks[category].map(t => t.id)) + 1;
   const getDefaultTrack = (category) => {
-    switch(category){
-      case "beats":
-      default: {
-        return "kick-electro01";
-      }
-    }
+    return samples[category][0];
   }
   const newTrack = {
     id,
@@ -104,7 +129,6 @@ export function muteTrack(tracks, id: number, category) {
 }
 
 export function updateTrackSample(tracks, id: number, sample: string, category) {
-
   const updatedInstrumentTracks = tracks[category].map(track => {
     if (track.id !== id) {
       return track;
@@ -151,19 +175,36 @@ export function decodeTracks(encodedTracks: EncodedTrack[]) {
 }
 
 export function randomTracks() {
-  const nT = Math.floor(3 + (Math.random() * 10));
-  return new Array(nT).fill().map((_, i) => {
-    return {
-      id: i + 1,
-      name: samples[Math.floor(Math.random() * samples.length)],
-      vol: Math.random(),
-      muted: false,
-      beats: initBeats(16).map(_ => Math.random() > .75),
-    }
-  });
+
+  const tracks = Object.keys(samples).reduce((acc, group) => {
+    const nT = Math.floor(2 + (Math.random() * (samples[group].length/10) ));
+    console.log("group", group, nT);
+    let groupTracks = [];  
+
+    new Array(nT).fill().forEach((_, i) => {
+      const trackName = samples[group][Math.floor(Math.random() * samples[group].length)];
+      if(groupTracks.includes(track => track.name === trackName)) {
+        return groupTracks;
+      }
+
+      const newTrack = {
+        id: i + 1,
+        name: trackName,
+        vol: Math.random(),
+        muted: false,
+        beats: initBeats(16).map(_ => Math.random() > .75),
+      }
+      groupTracks = [...groupTracks, newTrack];
+    });
+
+    return  {...acc, [group]: groupTracks}
+  }, {})
+
+  console.log("random tracks", tracks)
+  return tracks;
 }
 
-export function randomSong(): {bpm: number, tracks: Track[]} {
+export function randomSong() {
   return {
     bpm: Math.floor(Math.random() * 75) + 75,
     tracks: randomTracks(),
