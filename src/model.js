97fe2b2016f970974a5e4737ce31f6aae953aa37
Jakub Kozak <kozak.jakub55@gmail.com>
2018-04-16 17:35:49 +0200

    changed structure of tracks

--- src/model.js
+++ src/model.js
@@ -4,48 +4,66 @@ import type { Track, EncodedTrack } from "./types";
 import samples from "./samples.json";
 
 
-export function initTracks(): Track[] {
-  return [
-    {id: 1, name: "hihat-reso", vol: .4, muted: false, beats: initBeats(16)},
-    {id: 2, name: "hihat-plain", vol: .4, muted: false, beats: initBeats(16)},
-    {id: 3, name: "snare-vinyl01", vol: .9, muted: false, beats: initBeats(16)},
-    {id: 4, name: "kick-electro01", vol: .8, muted: false, beats: initBeats(16)},
-  ];
+export function initTracks() {
+  return {
+    "beats": [
+      {id: 1, name: "hihat-reso", vol: .4, muted: false, beats: initBeats(16)},
+      {id: 2, name: "hihat-plain", vol: .4, muted: false, beats: initBeats(16)},
+      {id: 3, name: "snare-vinyl01", vol: .9, muted: false, beats: initBeats(16)},
+      {id: 4, name: "kick-electro01", vol: .8, muted: false, beats: initBeats(16)},
+    ],
+    "guitar": [],
+    "drums": [],
+    "piano": []
+  }
 }
 
 export function initBeats(n: number): boolean[] {
   return new Array(n).fill(false);
 }
 
-export function addTrack(tracks: Track[]) {
-  const id = Math.max.apply(null, tracks.map(t => t.id)) + 1;
-  return [
-    ...tracks, {
-      id,
-      name: "kick-electro01",
-      vol: .8,
-      muted: false,
-      beats: initBeats(16),
+export function addTrack(tracks, category) {
+  const id = Math.max.apply(null, tracks[category].map(t => t.id)) + 1;
+  const getDefaultTrack = (category) => {
+    switch(category){
+      case "beats":
+      default: {
+        return "kick-electro01";
+      }
     }
-  ];
+  }
+  const newTrack = {
+    id,
+    name: getDefaultTrack(category),
+    vol: .8,
+    muted: false,
+    beats: initBeats(16),
+  }
+
+  return  {...tracks, [category]: [...tracks[category], newTrack]}
 }
 
-export function clearTrack(tracks: Track[], id: number): Track[] {
-  return tracks.map((track) => {
+export function clearTrack(tracks, id: number, category) {
+
+  const updatedInstrumentTracks = tracks[category].map(track => {
     if (track.id !== id) {
       return track;
     } else {
       return {...track, beats: initBeats(16)};
     }
   });
+
+  return {...tracks, [category]: updatedInstrumentTracks}
 }
 
-export function deleteTracks(tracks: Track[], id: number): Track[] {
-  return tracks.filter((track) => track.id !== id);
+export function deleteTracks(tracks, id: number, category) {
+  const updatedInstrumentTracks = tracks[category].filter((track) => track.id !== id);
+  return {...tracks, [category]: updatedInstrumentTracks};
 }
 
-export function toggleTrackBeat(tracks: Track[], id: number, beat: number): Track[] {
-  return tracks.map((track) => {
+export function toggleTrackBeat(tracks, id: number, beat: number, category) {
+
+  const updatedInstrumentTracks = tracks[category].map(track => {
     if (track.id !== id) {
       return track;
     } else {
@@ -55,36 +73,47 @@ export function toggleTrackBeat(tracks: Track[], id: number, beat: number): Trac
       };
     }
   });
+
+  return {...tracks, [category]: updatedInstrumentTracks}
 }
 
-export function setTrackVolume(tracks: Track[], id: number, vol: number): Track[] {
-  return tracks.map((track) => {
+export function setTrackVolume(tracks, id: number, vol: number, category) {
+
+  const updatedInstrumentTracks = tracks[category].map(track => {
     if (track.id !== id) {
       return track;
     } else {
       return {...track, vol};
     }
   });
+
+  return {...tracks, [category]: updatedInstrumentTracks}
 }
 
-export function muteTrack(tracks: Track[], id: number): Track[] {
-  return tracks.map((track) => {
+export function muteTrack(tracks, id: number, category) {
+
+  const updatedInstrumentTracks = tracks[category].map(track => {
     if (track.id !== id) {
       return track;
     } else {
       return {...track, muted: !track.muted};
     }
   });
+
+  return {...tracks, [category]: updatedInstrumentTracks}
 }
 
-export function updateTrackSample(tracks: Track[], id: number, sample: string): Track[] {
-  return tracks.map((track) => {
+export function updateTrackSample(tracks, id: number, sample: string, category) {
+
+  const updatedInstrumentTracks = tracks[category].map(track => {
     if (track.id !== id) {
       return track;
     } else {
       return {...track, name: sample};
     }
   });
+
+  return {...tracks, [category]: updatedInstrumentTracks}
 }
 
 function encodeBeats(beats: boolean[]): string {
@@ -95,19 +124,33 @@ function decodeBeats(encodedBeats: string): boolean[] {
   return encodedBeats.split("").map(beat => beat === "1");
 }
 
-export function encodeTracks(tracks: Track[]): EncodedTrack[] {
-  return tracks.map(({beats, ...track}) => {
-    return {...track, beats: encodeBeats(beats)};
+export function encodeTracks(tracks): EncodedTrack[] {
+  const encoded = Object.keys(tracks).map(category => {
+    return tracks[category].map(({beats, ...track}) => {
+      return {...track, beats: encodeBeats(beats)};
+    });
   });
+
+  console.log('encodeTracks', encoded);
+  return encoded;
 }
 
-export function decodeTracks(encodedTracks: EncodedTrack[]): Track[] {
-  return encodedTracks.map(({beats, ...encodedTrack}) => {
-    return {...encodedTrack, beats: decodeBeats(beats)};
+export function decodeTracks(encodedTracks: EncodedTrack[]) {
+  const decoded = Object.keys(encodedTracks).map(category => {
+    return encodedTracks[category].map(({beats, ...track}) => {
+      return {...track, beats: decodeBeats(beats)};
+    });
   });
+
+  console.log('decodeTracks', decoded);
+  return decoded;
+
+  // return encodedTracks.map(({beats, ...encodedTrack}) => {
+  //   return {...encodedTrack, beats: decodeBeats(beats)};
+  // });
 }
 
-export function randomTracks(): Track[] {
+export function randomTracks() {
   const nT = Math.floor(3 + (Math.random() * 10));
   return new Array(nT).fill().map((_, i) => {
     return {
